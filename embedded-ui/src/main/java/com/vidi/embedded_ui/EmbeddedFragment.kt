9741e65fb8ba930be9510adea31cdcf12b239cc3
Vincenzo Di Palma <vincenzo.dipalma@alten.it>
2020-01-09 15:35:59 +0100

    printing package name

--- embedded-ui/src/main/java/com/vidi/embedded_ui/EmbeddedFragment.kt
+++ embedded-ui/src/main/java/com/vidi/embedded_ui/EmbeddedFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class EmbeddedFragment : Fragment() {
@@ -13,7 +14,10 @@ class EmbeddedFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.embedded_fragment, container, false)
+        val v = inflater.inflate(R.layout.embedded_fragment, container, false)
+        val textView = v.findViewById<TextView>(R.id.the_text)
+        textView.text = requireContext().applicationContext.packageName
+        return v
     }
 
 }
